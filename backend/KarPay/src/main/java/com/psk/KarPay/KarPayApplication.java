8c5c40c089983191e37f675b01f16729f05f58a5
karpritam <karpritam194@gmail.com>
2025-08-31 23:39:52 +0530

    backebd folder created

---
+++ backend/KarPay/src/main/java/com/psk/KarPay/KarPayApplication.java
@@ -0,0 +1,13 @@
+package com.psk.KarPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarPayApplication.class, args);
+	}
+
+}
